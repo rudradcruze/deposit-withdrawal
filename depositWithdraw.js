2993ea57ba142c95cf37a817806473041fff7e4f
rudradcruze <francisrudra@gmail.com>
2022-09-03 00:21:44 +0600

    Withdraw added

--- depositWithdraw.js
+++ depositWithdraw.js
@@ -1,7 +1,7 @@
-const balance = document.getElementById('balance');
-const balanceAmount = parseFloat(balance.innerText);
 
 document.getElementById('buttonDeposit').addEventListener('click', function(){
+    const balance = document.getElementById('balance');
+    const balanceAmount = parseFloat(balance.innerText);
     // For deposit input field
     const depositField = document.getElementById('depositAmount');
     const depositFieldValue = depositField.value;
@@ -17,8 +17,38 @@ document.getElementById('buttonDeposit').addEventListener('click', function(){
     
     const depositAmount = parseFloat(depositFieldValue);
     depositDisplay.innerText = deposit + depositAmount;
-    balance.innerText = balanceAmount + depositAmount;
+    const balanceAmountCalculate = balanceAmount + depositAmount;
+    balance.innerText = balanceAmountCalculate;
 
     depositField.value = '';
 })
 
+document.getElementById('buttonWithdraw').addEventListener('click', function(){
+
+    const balance = document.getElementById('balance');
+    const balanceAmount = parseFloat(balance.innerText);
+
+    const withdrawField = document.getElementById('withdrawAmount');
+    const withdrawDisplay = document.getElementById('withdraw');
+    const withdraw = parseFloat(withdrawDisplay.innerText);
+    
+    if (withdrawField.value === '') {
+        withdrawField.value = '';
+        return;
+    }
+
+    const withdrawAmount = parseFloat(withdrawField.value);
+
+    if (withdrawAmount > balanceAmount)
+    {
+        alert("please withdraw less then or equal " + balanceAmount + " this amount");
+        withdrawField.value = '';
+        return;
+    }
+
+    let balanceAmountWithdrawCalculation = balanceAmount - withdrawAmount;
+    withdrawDisplay.innerText = withdrawAmount + withdraw;
+    withdrawField.value = '';
+    balance.innerText = balanceAmountWithdrawCalculation;
+
+})
